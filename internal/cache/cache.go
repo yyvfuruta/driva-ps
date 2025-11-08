b101c49db6aa73af5dbb73cb14fae60cdd45c1f3
yyvf <yyvf@mailbox.org>
2025-11-08 14:30:45 -0300

    feat: add cache on GET /orders/{id} using redis

---
+++ internal/cache/cache.go
@@ -0,0 +1,29 @@
+package cache
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/redis/go-redis/v9"
+)
+
+func NewConnection() (*redis.Client, error) {
+	host := os.Getenv("REDIS_HOST")
+	port := os.Getenv("REDIS_PORT")
+
+	envVars := map[string]string{
+		"REDIS_HOST": host,
+		"REDIS_PORT": port,
+	}
+
+	for key, value := range envVars {
+		if value == "" {
+			return nil, fmt.Errorf("%s environment variable not set", key)
+		}
+	}
+
+	url := fmt.Sprintf("%s:%s", host, port)
+	rdb := redis.NewClient(&redis.Options{Addr: url})
+
+	return rdb, nil
+}

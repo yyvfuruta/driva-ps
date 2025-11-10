581396f3f35032187eb160587cae89b8f7423732
yyvf <yyvf@mailbox.org>
2025-11-09 23:54:30 -0300

    fix: response of middleware not in JSON

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -10,24 +10,24 @@ func authMiddleware(next http.HandlerFunc) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		secretToken := os.Getenv("AUTH_TOKEN")
 		if secretToken == "" {
-			http.Error(w, "AUTH_TOKEN not set", http.StatusInternalServerError)
+			errorResponse(w, http.StatusIntervalServerError, "AUTH_TOKEN not set")
 			return
 		}
 
 		authHeader := r.Header.Get("Authorization")
 		if authHeader == "" {
-			http.Error(w, "Authorization header required", http.StatusUnauthorized)
+			errorResponse(w, http.StatusUnauthorized, "Authorization header required")
 			return
 		}
 
 		parts := strings.Split(authHeader, " ")
 		if len(parts) != 2 || parts[0] != "Bearer" {
-			http.Error(w, "Invalid Authorization header format", http.StatusUnauthorized)
+			errorResponse(w, http.StatusUnauthorized, "Invalid Authorization header format")
 			return
 		}
 
 		if parts[1] != strings.TrimSpace(secretToken) {
-			http.Error(w, "Invalid token", http.StatusUnauthorized)
+			errorResponse(w, http.StatusUnauthorized, "Invalid token")
 			return
 		}
 
